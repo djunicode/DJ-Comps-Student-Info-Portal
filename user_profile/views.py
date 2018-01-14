aa6aa3cc05c339c601a7febe343b880cd5b057ce
Ayush Kothari <aayushkothari11@yahoo.com>
2018-01-14 21:45:49 +0530

    Adding views for teacher login and register

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # from django.views import generic
 from django.contrib.auth import authenticate
 # from django.views.generic import View
-from .models import StudentProfile
+from .models import StudentProfile, TeacherProfile
 from django.http import HttpResponse
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -50,13 +50,60 @@ def user_login(request):
                     auth_login(request, user)
                     return render(request, 'user_profile/profile.html', {})
                 else:
-                    error = 'Your Rango account is disabled.'
+                    error = 'Your account is disabled.'
                     return render(request, 'user_profile/login.html', {'error': error})
             else:
                 error = 'Incorrect Username or Password'
                 return render(request, 'user_profile/login.html', {'error': error})
         else:
             return render(request, 'user_profile/login.html', {})
+
+
+def register_teacher(request):
+    if request.user.is_authenticated:
+        return render(request, 'user_profile/teacherprofile.html', {})
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('Sap_Id', '')
+            password = request.POST.get('password', '')
+            email = request.POST.get('email', '')
+            Sap_Id = request.POST.get('Sap_Id', '')
+
+            if User.objects.filter(username=username).exists():
+                error = 'The Sap_id is already in use by another account.'
+                return render(request, 'user_profile/registration_teacher.html', {'error': error})
+
+            else:
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.save()
+                teacher = TeacherProfile.objects.create(teacher=user, Sap_Id=Sap_Id)
+                teacher.save()
+                return render(request, 'user_profile/teacherprofile.html', {})
+        else:
+            return render(request, 'user_profile/registration_teacher.html', {})
+
+
+def user_login_teacher(request):
+    if request.user.is_authenticated:
+            return render(request, 'user_profile/teacherprofile.html', {})
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    auth_login(request, user)
+                    return render(request, 'user_profile/teacherprofile.html', {})
+                else:
+                    error = 'Your account is disabled.'
+                    return render(request, 'user_profile/teacher_login.html', {'error': error})
+            else:
+                error = 'Incorrect Username or Password'
+                return render(request, 'user_profile/teacher_login.html', {'error': error})
+        else:
+            return render(request, 'user_profile/teacher_login.html', {})
 
 
 def logout(request):
